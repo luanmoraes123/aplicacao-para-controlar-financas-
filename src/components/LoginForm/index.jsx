9e9e2e86ad5abf73e67eef62e18f965cf010a0cd
luan moraes <luan.moraees21@gmail.com>
2023-11-30 09:51:49 -0300

    tela de login finalizada

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -7,23 +7,51 @@ const LoginForm = () => {
 
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [notification, setNotification] = useState({
+    open: false,
+    message:'',
+    severity: ''
+  });
 
   const onSubmit = async (e) => {
     e.preventDefault();
     try {
       const res = await axios.post('http://localhost:8080/auth/login', {email, password});
       localStorage.setItem('token', res.data.data.token);
+      setNotification({
+        open: true,
+        message: 'Login efetuado com sucesso',
+        severity: 'success'
+      });
     } catch (error) {
       console.log(error);
+      setNotification({
+        open: true,
+        message: error.response.data.data,
+        severity: 'error'
+      });
     }
   }
 
   return(
     <S.Form action="" onSubmit={onSubmit}>
       <S.H1>Formulario de login</S.H1>
-      <S.TextField onChange={(e) => setEmail(e.target.value)} variant='outlined' type='text' name='email' label='E-mail' />
-      <S.TextField onChange={(e) => setPassword(e.target.value)} variant='outlined' type='password' name='password' label='Senha' />
+      <S.TextField onChange={(e) => setEmail(e.target.value)} variant='outlined' type='text' placeholder='E-mail' />
+      <S.TextField onChange={(e) => setPassword(e.target.value)} variant='outlined' type='password' placeholder='Senha' />
       <S.Button variant='contained' type="submit">Entrar</S.Button>
+      <S.Snackbar open={notification.open} autoHideDuration={3000} onClose={()=> setNotification({
+          open: false,
+          message:'',
+          severity:''
+        })}>
+          <S.Alert onClose={()=> setNotification({
+          open: false,
+          message:'',
+          severity:''
+        })} severity={notification.severity} sx={{ width: '100%' }}>
+           {notification.message}
+          </S.Alert>
+        </S.Snackbar>
     </S.Form>
   )
 }
--- src/components/LoginForm/style.jsx
+++ src/components/LoginForm/style.jsx
@@ -1,14 +1,20 @@
 import styled from '@emotion/styled'
 import ButtonMUI from '@mui/material/Button';
 import TextFieldMUI from '@mui/material/TextField';
+import SnackbarMUI from '@mui/material/Snackbar'
+import AlertMUI from '@mui/material/Alert'
 
-export const Button = styled(ButtonMUI)``
+export const Button = styled(ButtonMUI)`
+  
+`
 
 export const TextField = styled(TextFieldMUI)`
   background-color: #fff;
 `
 
-export const H1 = styled.h1 ``
+export const H1 = styled.h1 `
+  font-size: 25px;
+`
 
 export const Form = styled.form `
   display: flex;
@@ -17,3 +23,7 @@ export const Form = styled.form `
   gap: 10px;
   text-align: center;
 `
+
+export const Alert = styled(AlertMUI)``
+
+export const Snackbar = styled(SnackbarMUI)``
