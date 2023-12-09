63a95cf22cadf7e54552a9221f3b6045528986ee
luan moraes <luan.moraees21@gmail.com>
2023-12-09 14:54:41 -0300

    rota de dashboard iniciada

---
+++ src/app/(auth)/dashboard/page.jsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useEffect } from 'react';
+import axios from 'axios';
+
+export const DashBoard = () => {
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+  
+    return () => {
+      second
+    }
+  }, [])
+  
+  return(
+    <div></div>
+  )
+}
